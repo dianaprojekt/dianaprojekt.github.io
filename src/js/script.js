5fe7d6c4ded14b3fcf7d224597f961d1382f00b6
dianaprojekt <nobawelna@gmail.com>
2022-11-13 14:17:24 +0100

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,36 @@
 // }
 
 // mobileMenu();
+
+
+// alert('witaj świecie');
+
+// prompt('Podaj swoje imię', 'Tutaj wpisz imię');
+
+// const x = 12;
+// const bla = "tekst";
+// alert(bla);
+
+// const intro = "Podaj imię";
+// var wynik;
+// wynik = prompt(intro);
+// alert(wynik);
+
+
+// var a = 4;
+// var b = 3;
+// wynik = (a == b) ? 'Zmienna są równe' : 'Zmienne mają różne wartości';
+// alert(wynik);
+
+// for (i = 1; i <= 5; i++)
+// {
+//     alert(i);
+// }
+
+
+// function moja_funk(a, b, c) {
+// 	z = a+b;
+// 	z = z/c;
+// 	return z;
+// }
+// alert(moja_funk(2, 4, 7));
